2d7cbf5bc457c3d5d675e491896b0909caf8e7df
Abagail Hudson <abagailanne@gmail.com>
2019-03-28 23:53:51 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray{
+
+}
+function destructivelyAddElementToEndOfArray{
+
+}
+function addElementToEndOfArray{
+
+}
+function destructivelyAddElementToEndOfArray{
+
+}
