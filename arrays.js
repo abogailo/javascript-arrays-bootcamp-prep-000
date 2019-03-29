6618be55351762a982f714eb10f94c777d4a893e
Abagail Hudson <abagailanne@gmail.com>
2019-03-29 00:12:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
 function addElementToBeginningOfArray(array, element){
-  var array = [element]
-  cities.unshift(element)
+  return [element, ...array];
 }
